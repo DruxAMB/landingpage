007caa4b8dd68eeba6b489c335e3695548de896d
DruxAMB <druxambmarketing@gmail.com>
2025-04-19 23:34:13 +0000

    fixed build error

--- components/home/Join.tsx
+++ components/home/Join.tsx
@@ -6,8 +6,7 @@ export const Join = () => {
     <section className="w-full bg-white py-16 px-4 flex flex-col items-center justify-center text-center">
       {/* Headline */}
       <h2 className="text-[#B30202] text-3xl md:text-5xl font-extrabold mb-6 md:mb-6 max-w-3xl mx-auto">
-        Chaapa Ride is transforming <br className="hidden md:block" />
-        Africa’s mobility industry.
+        Chaapa Ride is transforming <br className="hidden md:block" /> Africa&apos;s mobility industry.
       </h2>
       {/* Subheadline */}
       <div className="text-gray-500 text-base md:text-lg font-medium mb-10">One Ride at a time.</div>
